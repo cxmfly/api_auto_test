7a28b0f8b4e51623bae382b3deaa94eca3705cfb
cxmfly <470070654@qq.com>
2022-01-20 08:01:13 +0800

    更改日志输出到allure

--- src/test/java/com/common/GlobalConfig.java
+++ src/test/java/com/common/GlobalConfig.java
@@ -2,7 +2,7 @@ package com.common;
 
 public class GlobalConfig {
     //调试开关  true调试模式（日志输出到控制台）   false生产模式（日志输出到allure）
-    public static final boolean IS_DEBUG=true;
+    public static final boolean IS_DEBUG=false;
     //项目url地址
     public static final String url="http://mall.lemonban.com:8107";
 //    public static final String url="http://erp.lemfix.com";
